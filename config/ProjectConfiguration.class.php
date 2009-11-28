8af40223aec34a99977e5099fc33f84c756ffbfe
Pedro Hernandez <phpleo@gmail.com>
2009-11-27 23:06:20 -0500

    . agregado el esqueleto de la aplicaion

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/opt/symfony/sf1.4/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
